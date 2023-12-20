f18f3253a47e4cec6fa44626a86ab1d8c4a21cc9
ntokozo-sithebe <ntoky28@gmail.com>
2023-12-20 08:10:47 +0200

    Dawg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link>|
+    <router-link to="/resume">Resume</router-link>|
+    <router-link to="/projects">Project</router-link>|
+    <router-link to="/testimonials">Testimonials</router-link>|
+    <router-link to="/Contact">Contact</router-link>|
+    
+
   </nav>
   <router-view/>
 </template>
